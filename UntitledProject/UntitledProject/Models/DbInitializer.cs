174b66862a3a25670ce02e992114066b21eab10a
Papp Richárd <papprichard97@gmail.com>
2020-11-02 12:27:28 +0100

    db seed

--- UntitledProject/UntitledProject/Models/DbInitializer.cs
+++ UntitledProject/UntitledProject/Models/DbInitializer.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +16,62 @@ namespace UntitledProject.Models
         {
             if (!(context.Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
             {
+                var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var UserManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
                 context.Database.Migrate();
+                if (!context.Roles.Any())
+                {
+                    string[] roleNames = { "Admin", "User" };
+                    IdentityResult roleResult;
+
+                    foreach (var roleName in roleNames)
+                    {
+                        var roleExist = RoleManager.RoleExistsAsync(roleName);
+
+                        if (!roleExist.Result)
+                        {
+
+                            roleResult = RoleManager.CreateAsync(new IdentityRole(roleName)).Result;
+                        }
+                    }
+                }
+
+                if (context.AppUser.Any())
+                {
+                    return;   // DB már fel van töltve
+                }
+                else
+                {
+
+                    var poweruser = new AppUser
+                    {
+                        UserName = "admin@admin.hu",
+                        Email = "admin@admin.hu",
+                        EmailConfirmed = true,
+                        PhoneNumber = "01234567890",
+                        PhoneNumberConfirmed = true,
+                        CreatedDate = DateTime.Now,
+
+                    };
+                    string adminPassword = "Qwe_123";
+
+                    var createPowerUser = UserManager.CreateAsync(poweruser, adminPassword);
+                    if (createPowerUser.Result.Succeeded)
+                    {
+
+                        IdentityResult result = UserManager.AddToRoleAsync(poweruser, "Admin").Result;
+
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception("Sikertelen db inicializálás !");
+                        }
+
+                    }
+                }
+
             }
+
+
         }
     }
 }
